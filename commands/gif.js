2b6fbdefb199b3df4eb2d5d31924b84d3d842681
cybemachine <80441770+cybemachine@users.noreply.github.com>
2021-08-01 20:36:09 +0545

    added gif funcationality

--- commands/gif.js
+++ commands/gif.js
@@ -1,8 +1,15 @@
+const fetch = require('node-fetch')
+var limit = 8;
 module.exports = {
     name: 'gif',
     description: 'gif!',
-    execute(message, args) {
-        console.log('gif');
-        message.channel.send('gif.');
+    async execute(message, args) {
+        var host = 'https://g.tenor.com/v1/search?'
+        var query = `${host}q=${args[0]}&key=${process.env.TENORKEY}&limit=${limit}`
+        const res = await fetch(query);
+        const data = await res.json();
+        const index = Math.floor(Math.random() * limit);
+        const now = data.results[index].media[0].gif.url;
+        message.channel.send(now)
     },
 };
