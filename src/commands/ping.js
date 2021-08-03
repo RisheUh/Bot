0c1070d80f31bc1bac50a501b2d63d9874f0aee4
cybemachine <80441770+cybemachine@users.noreply.github.com>
2021-08-03 19:48:15 +0545

    modified ping ready ping: adde 0:00 S ready: added � and added proper activity

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,3 +1,4 @@
+const { client } = require('../index')
 module.exports = {
     name: 'ping',
     description: 'get\'s latency',
@@ -6,10 +7,20 @@ module.exports = {
     guildOnly: false,
     args: false,
     execute(message, args) {
-        var d = Date.now() - message.createdTimestamp
-        if (d < 0) {
-
+        function sp(millis) {
+            var minutes = Math.floor(millis / 60000);
+            var seconds = ((millis % 60000) / 1000).toFixed(0);
+            return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
         }
-        message.channel.send(`Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+
+        var d = (message.createdTimestamp - client.ws.ping) - Date.now()
+
+        if (d > 999) {
+            message.channel.send(
+                `Latency is ${sp(d)} s\nAPI Latency is ${Math.round(client.ws.ping)} ms`
+            )
+        } else {
+            message.channel.send(`Latency is ${d}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+        }
     },
 };
--- src/events/ready.js
+++ src/events/ready.js
@@ -2,7 +2,7 @@ module.exports = {
     event: 'ready',
     once: true,
     run(client) {
-        client.user.setActivity(`on ${client.guilds.cache.size} servers`);
-        console.log('Bot on!');
+        client.user.setActivity(`${client.guilds.cache.size} servers`, { type: 'LISTENING' });
+        console.log('👋');
     },
 };
